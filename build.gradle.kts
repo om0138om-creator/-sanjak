255d68ac7e9ddfecdfa3f77858f5dc90365ccb23
om0138om-creator <om0138om@gmail.com>
2026-03-23 23:01:34 +0200

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+// build.gradle.kts (Project Root)
+// تم تصميم التطبيق بواسطة عمر سنجق
+
+plugins {
+    id("com.android.application") version "8.2.2" apply false
+    id("com.android.library") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "1.9.22" apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.layout.buildDirectory)
+}
